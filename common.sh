915afb1fe238c605ce468e930f4c9f26890273ea
srujanakamarsu <srujanamythree@gmail.com>
2023-07-25 13:16:36 +0530

    common.sh updated

--- common.sh
+++ common.sh
@@ -1,3 +1,5 @@
+log=/tmp/roboshop.log
+
 func_apppreq()
 {
     echo -e  "\e[36m>>>>>> create application user <<<<<<\e[0m" | tee -a /tmp.roboshop.log
@@ -59,23 +61,22 @@ func_nodejs()
 func_java()
 {
     echo -e  "\e[36m>>>>>> create ${component} service <<<<<<\e[0m"
-    cp ${component}.service /etc/systemd/system/${component}.service
+    cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
 
     echo -e  "\e[36m>>>>>> install maven <<<<<<\e[0m"
-    yum install maven -y
+    yum install maven -y &>>${log}
 
     func_apppreq
 
     echo -e  "\e[36m>>>>>> build ${component} service <<<<<<\e[0m"
-    cd /app 
-    mvn clean package 
-    mv target/${component}-1.0.jar ${component}.jar 
+    mvn clean package &>>${log}
+    mv target/${component}-1.0.jar ${component}.jar &>>${log}
 
     echo -e  "\e[36m>>>>>> install mysql client <<<<<<\e[0m"
-    yum install mysql -y 
+    yum install mysql -y &>>${log}
 
     echo -e  "\e[36m>>>>>> load schema <<<<<<\e[0m"
-    mysql -h mysql.vyshu.online -uroot -pRoboShop@1 < /app/schema/${component}.sql
+    mysql -h mysql.vyshu.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
 
     func_systemd
 }
